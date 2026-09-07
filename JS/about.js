1ec17ab3e61e13728b940149e52dbfcf0a81615c
Wheelythechair <simon.lapinskas@gmail.com>
2026-09-07 19:35:04 +0300

    finished flip cards

--- JS/about.js
+++ JS/about.js
@@ -1,24 +1,37 @@
 "use strict"
 
 
-const buttonList = document.querySelectorAll(".d_grid button")
 
-console.log(buttonList)
+const directors = document.querySelector(".directors");
 
-buttonList[0].onclick = function(event) {
-    event.stopPropagation();
-    if (event.target.parentElement.style.zIndex == Number("-100")){
-        console.log(event.target.parentElement.style.zIndex)
-        event.target.parentElement.style.zIndex = "100"
-        console.log(event.target.parentElement.style.zIndex)
-    } else {
-        event.target.parentElement.style.zIndex = "-100"
+
+
+
+directors.onclick = function(event) {
+
+    if (event.target.nodeName == "BUTTON" && event.target.parentElement.classList.contains("not_flipped")){
+        let className = event.target.parentElement.className.split(" ");
+        document.querySelector(`.${className[0]}`).classList.remove("not_flipped")
+        document.querySelector(`.${className[0]}`).classList.add("flipped")
+    } else if (event.target.nodeName == "BUTTON" && event.target.parentElement.classList.contains("flipped")){
+        let className = event.target.parentElement.className.split(" ");
+        document.querySelector(`.${className[0]}`).classList.remove("flipped")
+        document.querySelector(`.${className[0]}`).classList.add("not_flipped")
     }
 
-    event.target.parentElement.style.zIndex = "-100"
-    console.log(event.target.parentElement)
-}
 
+if (event.target.parentElement.nodeName == "BUTTON" && event.target.parentElement.parentElement.classList.contains("not_flipped")){
+        let className = event.target.parentElement.parentElement.className.split(" ");
+        document.querySelector(`.${className[0]}`).classList.remove("not_flipped")
+        document.querySelector(`.${className[0]}`).classList.add("flipped")
+    } else if (event.target.parentElement.nodeName == "BUTTON" && event.target.parentElement.parentElement.classList.contains("flipped")){
+        let className = event.target.parentElement.parentElement.className.split(" ");
+        document.querySelector(`.${className[0]}`).classList.remove("flipped")
+        document.querySelector(`.${className[0]}`).classList.add("not_flipped")
+    }
+    
+    
+}
 
 
 
