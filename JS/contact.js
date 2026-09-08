137a02ea7a9ab4f62edd2aed480f9ac38e836ffd
Zigmas-Petrauskas <zigmasdev@gmail.com>
2026-09-08 15:57:29 +0300

    complete contact form active states

--- JS/contact.js
+++ JS/contact.js
@@ -2,17 +2,22 @@
 
 // Kontaktų formos validacija
 
+// Surandama kontaktų forma
 const contactForm = document.querySelector("#contact-form");
 
+// Tikrinama ar kontaktų forma egsituoja
 if (contactForm) {
+  // Surandami visi formos laukų blokai
   const formFields = contactForm.querySelectorAll(".form-field");
 
+  // Formos laukų tikrinimas paspaudus Submit
   contactForm.addEventListener("submit", (event) => {
     let hasError = false;
 
     formFields.forEach((field) => {
       const input = field.querySelector("input");
 
+      // Tuščiam laukui pridedama klaidos klasė
       if (input.value.trim() === "") {
         field.classList.add("error");
         hasError = true;
@@ -21,11 +26,13 @@ if (contactForm) {
       }
     });
 
+    // Forma nepateikiama jei yra neužpildytų laukų
     if (hasError) {
       event.preventDefault();
     }
   });
 
+  // Klaidos būsena pašalinama pradėjus pildyti formą
   formFields.forEach((field) => {
     const input = field.querySelector("input");
 
